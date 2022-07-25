8a00bd04133ed7a50a377a3a534fa9a27fc7e4dd
miko-github <mikoloism-github@gmail.com>
2022-07-25 13:38:43 +0430

    feat (Server.js) : Using default express middleware and HTTP Logger middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const httpLogger = require('./middleware/http-logger.js');
 const app = express();
+
+/* USE MIDDLEWARE */
+app.use(httpLogger());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 /* LISTENER AND EXPORTS */
